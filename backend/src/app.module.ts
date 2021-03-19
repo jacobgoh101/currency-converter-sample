64238f55def131d09b87500323a6a7695f02eb35
Jacob Goh <jacob.goh@123rf.com>
2021-03-19 22:15:24 +0800

    setup base exchange rate module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExchangeRateModule } from './exchange-rate/exchange-rate.module';
 
 @Module({
-  imports: [],
+  imports: [ExchangeRateModule],
   controllers: [AppController],
   providers: [AppService],
 })
