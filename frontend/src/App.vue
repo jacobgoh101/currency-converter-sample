1b65169c66a0fe2e7eb5c2d18d3351b20c353e5a
Jacob Goh <jacob.goh@123rf.com>
2021-03-20 23:10:06 +0800

    Add converter to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,164 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <section class="section">
+      <div class="columns">
+        <div class="column">
+          <b-field label="Amount">
+            <b-numberinput
+              v-model="amount"
+              expanded
+              min-step="0.01"
+            ></b-numberinput>
+          </b-field>
+        </div>
+        <div class="column">
+          <b-field label="Source">
+            <b-select
+              placeholder="Select the source currency"
+              v-model="from"
+              expanded
+            >
+              <option
+                v-for="option in options"
+                :value="option.value"
+                :key="option.value"
+              >
+                {{ option.label }}
+              </option>
+            </b-select>
+          </b-field>
+        </div>
+        <div class="column">
+          <b-field label="Target">
+            <b-select
+              placeholder="Select the target currency"
+              v-model="to"
+              expanded
+            >
+              <option
+                v-for="option in options"
+                :value="option.value"
+                :key="option.value"
+              >
+                {{ option.label }}
+              </option>
+            </b-select>
+          </b-field>
+        </div>
+      </div>
+    </section>
+    <div class="columns" v-if="exchangeRateError">
+      <div class="column">
+        <b-notification
+          type="is-danger"
+          aria-close-label="Close notification"
+          role="alert"
+        >
+          {{ exchangeRateError }}
+        </b-notification>
+      </div>
     </div>
-    <router-view />
+    <section class="section has-text-centered" :class="$style.resultSection">
+      <b-loading
+        :is-full-page="false"
+        v-model="isValidating"
+        :can-cancel="true"
+      />
+      <template v-if="convertedAmount">
+        <div class="has-text-5">{{ amount }} {{ from }} = <br /></div>
+        <div>
+          <span class="is-size-1">{{ formatAmount(convertedAmount) }}</span>
+          {{ to }}
+        </div>
+        <div class="is-size-6">
+          1 {{ to }} = {{ formatAmount(1 / rate) }} {{ from }}
+        </div>
+        <div class="is-size-6">
+          1 {{ from }} = {{ formatAmount(rate) }} {{ to }}
+        </div>
+      </template>
+    </section>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { computed, defineComponent, ref } from "@vue/composition-api";
+import useSWRV from "swrv";
+import axios from "axios";
+
+interface Option {
+  label: string;
+  value: string;
 }
 
-#nav {
-  padding: 30px;
+interface ExchangeRate {
+  from: string;
+  to: string;
+  rate: number;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+export default defineComponent({
+  setup() {
+    const options: Option[] = [
+      { label: "USD", value: "USD" },
+      { label: "EUR", value: "EUR" },
+      { label: "MYR", value: "MYR" },
+    ];
+    const amount = ref(1);
+    const from = ref<Option["value"]>(options[0].value);
+    const to = ref<Option["value"]>(options[1].value);
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+    const {
+      data: exchangeRateData,
+      error: exchangeRateError,
+      isValidating,
+    } = useSWRV<ExchangeRate>(
+      () => from.value && to.value && [from.value, to.value],
+      (f: Option["value"], t: Option["value"]) => {
+        return axios
+          .get(`http://localhost:3000/exchange-rates?from=${f}&to=${t}`)
+          .then((res) => res.data);
+      },
+      { shouldRetryOnError: false, errorRetryCount: 0 }
+    );
+
+    const convertedAmount = computed<number | null>(() => {
+      return amount.value && exchangeRateData.value?.rate
+        ? amount.value * exchangeRateData.value?.rate
+        : null;
+    });
+
+    const rate = computed<number | undefined>(() => {
+      return exchangeRateData.value?.rate;
+    });
+
+    const formatAmount = (num: number) => {
+      return num?.toLocaleString("en-US", { maximumFractionDigits: 6 });
+    };
+
+    return {
+      amount,
+      options,
+      from,
+      to,
+      exchangeRateData,
+      exchangeRateError,
+      convertedAmount,
+      isValidating,
+      rate,
+      formatAmount,
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+@import "~@mdi/font/css/materialdesignicons.min.css";
+</style>
+
+<style lang="scss" module>
+.resultSection {
+  min-height: 200px;
+  position: relative;
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueCompositionAPI from '@vue/composition-api';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
+Vue.use(VueCompositionAPI);
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
+Vue.prototype.log = console.log;
 
 new Vue({
   router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
